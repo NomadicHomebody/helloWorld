964981f578e677dd40be7a4bd02e06f138da4f4f
Jamie Spinner <jspinn.12@gmail.com>
2025-02-25 22:45:17 -0500

    Enhance /test123 endpoint response with custom headers

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
+
+
 @app.route('/test123', methods=['GET'])
 def test_endpoint():
-    return jsonify({"message": "Hello from the /test123 endpoint!"})
+    response = make_response(jsonify({"message": "Hello from the /test123 endpoint!"}))
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Correlation-Id'] = '1234567890'
+    return response, 200
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8099, debug=True)

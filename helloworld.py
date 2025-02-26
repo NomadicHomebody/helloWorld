c343698b96b4667e80efd15fd95c0c96ec6493d4
Jamie Spinner <jspinn.12@gmail.com>
2025-02-25 22:24:00 -0500

    Add Flask web server with a test endpoint and requirements file

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print("sample text")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/test123', methods=['GET'])
+def test_endpoint():
+    return jsonify({"message": "Hello from the /test123 endpoint!"})
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8099, debug=True)
